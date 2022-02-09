d152ce23c7a06dfa7fe5a1490bc055ad8317a5c8
AbubakarMzf <abubakar.mzf@gmail.com>
2022-02-09 16:44:05 +0100

    partie 2

---
+++ src/Launcher.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Input :");
+            String input = sc.nextLine();
+            switch (input) {
+                case "quit":
+                    return;
+                default:
+                    System.out.println("Unknown command");
+            }
+    }
+}
