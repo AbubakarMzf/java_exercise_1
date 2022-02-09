d2b68a90f33bdbffe22a220c5786b39f9d70f8e2
AbubakarMzf <abubakar.mzf@gmail.com>
2022-02-09 16:51:19 +0100

    partie 4

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class Launcher {
 
+    static int fibo(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+
     public static void main(String[] args) {
         while(true) {
             Scanner sc = new Scanner(System.in);
@@ -10,6 +17,12 @@ public class Launcher {
             switch (input) {
                 case "quit":
                     return;
+                case "fibo":
+                    System.out.println("Enter number :");
+                    int nb = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println("Fibo : " + fibo(nb));
+                    break;
                 default:
                     System.out.println("Unknown command");
             }
