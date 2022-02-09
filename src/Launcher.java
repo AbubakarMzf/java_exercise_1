50b6d295b24332daa39617d3d92edfd5570263e2
AbubakarMzf <abubakar.mzf@gmail.com>
2022-02-09 16:45:52 +0100

    partie 3

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Launcher {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        while(true) {
+            Scanner sc = new Scanner(System.in);
             System.out.println("Input :");
             String input = sc.nextLine();
             switch (input) {
@@ -12,5 +13,6 @@ public class Launcher {
                 default:
                     System.out.println("Unknown command");
             }
+        }
     }
 }
