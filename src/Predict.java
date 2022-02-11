49239c89fbe95edeb670d174ea56a1be8b05107b
AbubakarMzf <abubakar.mzf@gmail.com>
2022-02-11 11:59:34 +0100

    aaa

--- src/Predict.java
+++ src/Predict.java
@@ -2,10 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Predict implements Command{
 
@@ -20,7 +17,7 @@ public class Predict implements Command{
         Path filePath = Paths.get(path);
         try {
             String content = Files.readString(filePath);
-            String[] word_list = content.split(" ");
+            String[] word_list = content.toLowerCase().split(" ");
             System.out.println("Enter a word :");
             String mot = sc.nextLine();
             String phrase = mot;
@@ -34,9 +31,6 @@ public class Predict implements Command{
                     }
                 }
                 Collections.sort(mot_apres);
-                //System.out.println("liste des mots apres " + mot_apres);
-
-
                 int maxi = 1;
                 String mot_max = mot_apres.get(mot_apres.size() - 1) ;
                 int counte = 1;
@@ -59,28 +53,6 @@ public class Predict implements Command{
                 //System.out.println("j'ai hoisis le mot : " + mot_max);
                 phrase += " " + mot_max;
                 mot= mot_max;
-/*
-                String curr = mot_apres.get(0);
-                int max = 0;
-                int count = 1;
-
-                for (int m = 1; m < mot_apres.size(); m++) {
-                    if (mot_apres.get(m) != curr){
-                        if (count > max){
-                            curr = mot_apres.get(m - 1);
-                            max = count;
-                        }
-                        count = 0;
-                    }
-                    else{
-                        count += 1;
-                    }
-                }*/
-
-                //System.out.println("current : " + curr);
-                //phrase += " " + mot_max;
-                //mot = curr;
-
             }
             System.out.println(phrase);
         }
